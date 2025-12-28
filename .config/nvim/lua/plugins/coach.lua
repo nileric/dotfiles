a69635cc7feb4d80b14e9334e75e80ba94e6a9f1
nileric <kvashin.am98@gmail.com>
2025-12-28 22:32:36 +0700

    update coach plugin keymap

--- .config/nvim/lua/plugins/coach.lua
+++ .config/nvim/lua/plugins/coach.lua
@@ -7,6 +7,6 @@ return {
     require("vim-coach").setup()
   end,
   keys = {
-    { "<leader>?", "<cmd>VimCoach<cr>", desc = "Vim Coach" },
+    { "<leader>z", "<cmd>VimCoach<cr>", desc = "Vim Coach" },
   },
 }
