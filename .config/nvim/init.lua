f7c8d20bb30d08fa2e6ef283488953cc70971037
nileric <kvashin.am98@gmail.com>
2026-02-05 21:28:27 +0700

    [nvim] ghostty title

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+if vim.fn.getenv("TERM_PROGRAM") == "ghostty" then
+  vim.opt.title = true
+  vim.opt.titlestring = "%{fnamemodify(getcwd(), ':t')}"
+end
