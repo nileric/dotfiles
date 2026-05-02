f9a98bc2e7763a83c40dda8384ded1ccbfb4257f
nileric <kvashin.am98@gmail.com>
2026-05-03 00:22:19 +0700

    [nvim] fix refactoring-nvim. again

---
+++ .config/nvim/lua/plugins/refactoring-nvim.lua
@@ -0,0 +1,11 @@
+return {
+  "ThePrimeagen/refactoring.nvim",
+  dependencies = {
+    "lewis6991/async.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("refactoring").setup({})
+  end,
+}
