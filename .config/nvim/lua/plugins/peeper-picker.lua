b761f2ab16da47c95a3083cf29486d4b03dbfb41
nileric <kvashin.am98@gmail.com>
2026-05-31 21:25:05 +0700

    [nvim] try wayfinder alternative plugin

---
+++ .config/nvim/lua/plugins/peeper-picker.lua
@@ -0,0 +1,9 @@
+return {
+  "parwest/peeper-picker.nvim",
+  main = "peeper_picker",
+  cmd = "PeeperPicker",
+  opts = {},
+  keys = {
+    { "<leader>cp", "<cmd>PeeperPicker<cr>", desc = "Peeper Picker" },
+  },
+}
