817ccf03b795133fbe940dfa74dfa11b87169da5
nileric <kvashin.am98@gmail.com>
2026-06-01 22:34:54 +0700

    [nvim] [ghostty] fix clipboard on linux

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-if vim.fn.getenv("TERM_PROGRAM") == "ghostty" then
-  vim.opt.title = true
-  vim.opt.titlestring = "%{fnamemodify(getcwd(), ':t')}"
-end
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,3 +5,24 @@
 local opt = vim.opt
 opt.relativenumber = true
 opt.clipboard = "unnamedplus"
+
+if vim.fn.getenv("TERM_PROGRAM") == "ghostty" then
+  vim.opt.title = true
+  vim.opt.titlestring = "%{fnamemodify(getcwd(), ':t')}"
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = function()
+      return vim.fn.getreg("+")
+    end,
+    ["*"] = function()
+      return vim.fn.getreg("*")
+    end,
+  },
+}
