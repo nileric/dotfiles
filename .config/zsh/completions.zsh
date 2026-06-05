20dfbe652eed9817829deb2d4777d05bf8ca1e89
nileric <kvashin.am98@gmail.com>
2026-06-05 09:45:30 +0700

    [zsh] fix completitions. again

--- .config/zsh/completions.zsh
+++ .config/zsh/completions.zsh
@@ -1,40 +1,34 @@
 #!/bin/zsh
-
-autoload -Uz compinit
-
 if [[ "$OSTYPE" == darwin* ]]; then
   ZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/Library/Caches}/zsh"
 else
   ZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/zsh"
 fi
-
 mkdir -p "$ZSH_CACHE_DIR/completions"
 
-ZCOMPDUMP="$ZSH_CACHE_DIR/zcompdump-${ZSH_VERSION}"
+# generate completion files BEFORE compinit. like _kubectl in fpath
+if command -v kubectl &>/dev/null; then
+  KCOMP="$ZSH_CACHE_DIR/completions/_kubectl"
+  if [[ ! -f "$KCOMP" || "$(command -v kubectl)" -nt "$KCOMP" ]]; then
+    kubectl completion zsh >| "$KCOMP"
+  fi
+fi
 
-# Rebuild compdump if older than 24h.
+if command -v docker &>/dev/null; then
+  DCOMP="$ZSH_CACHE_DIR/completions/_docker"
+  if [[ ! -f "$DCOMP" || "$(command -v docker)" -nt "$DCOMP" ]]; then
+    docker completion zsh >| "$DCOMP" 2>/dev/null
+  fi
+fi
+
+# add to fpath BEFORE compinit
+fpath=("$ZSH_CACHE_DIR/completions" $fpath)
+
+autoload -Uz compinit
+ZCOMPDUMP="$ZSH_CACHE_DIR/zcompdump-${ZSH_VERSION}"
 if [[ -n "$ZCOMPDUMP"(#qN.mh+24) ]]; then
   compinit -d "$ZCOMPDUMP"
 else
   compinit -C -d "$ZCOMPDUMP"
 fi
 
-if command -v kubectl &>/dev/null; then
-  KUBECTL_COMPLETION="$ZSH_CACHE_DIR/completions/kubectl_completion"
-
-  if [[ ! -f "$KUBECTL_COMPLETION" || "$(command -v kubectl)" -nt "$KUBECTL_COMPLETION" ]]; then
-    kubectl completion zsh >| "$KUBECTL_COMPLETION"
-  fi
-
-  source "$KUBECTL_COMPLETION"
-fi
-
-if command -v docker &>/dev/null; then
-  DOCKER_COMPLETION="$ZSH_CACHE_DIR/completions/docker_completion"
-
-  if [[ ! -f "$DOCKER_COMPLETION" || "$(command -v docker)" -nt "$DOCKER_COMPLETION" ]]; then
-    docker completion zsh >| "$DOCKER_COMPLETION"
-  fi
-
-  source "$DOCKER_COMPLETION"
-fi
