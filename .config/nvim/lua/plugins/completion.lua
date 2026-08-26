a4a444cc6aa677dbd946a181988b952b8e025cfd
nileric <kvashin.am98@gmail.com>
2026-08-26 20:49:57 +0700

    [nvim] rework blink.cmp

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,23 +1,35 @@
 return {
   "saghen/blink.cmp",
-  opts = function(_, opts)
-    vim.b.completion = true
-    opts.keymap = {
-      preset = "enter",
+  opts = {
+    keymap = {
+      preset = "default",
+
+      -- Использование Enter только для переноса строки, ЕСЛИ элемент не выбран вручную
+      ["<CR>"] = { "accept", "fallback" },
+
+      -- Ручной вызов/закрытие всплывающего окна
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide" },
+
+      -- Навигация с помощью Tab / Shift+Tab или Ctrl+N / Ctrl+P
       ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
       ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
-    }
+    },
+    completion = {
+      list = {
+        selection = {
+          preselect = false,
+          auto_insert = false,
+        },
+      },
+      trigger = {
+        show_in_snippet = false,
+      },
 
-    Snacks.toggle({
-      name = "Completion",
-      get = function()
-        return vim.b.completion
-      end,
-      set = function(state)
-        vim.b.completion = state
-      end,
-    }):map("<leader>uk")
-
-    return opts
-  end,
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 500,
+      },
+    },
+  },
 }
