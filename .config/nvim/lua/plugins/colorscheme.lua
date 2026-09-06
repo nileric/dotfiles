b3c6a9f0f32edf207586291dbdf3610cb6b81d3d
nileric <kvashin.am98@gmail.com>
2026-09-06 17:04:33 +0700

    [nvim] fix auto theme switch

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,17 @@
 return {
   {
     "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+      light_style = "day",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
   },
 }
