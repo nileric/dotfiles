c78f2f38bcba50d31f559599f58b20b4b61df5f4
nileric <kvashin.am98@gmail.com>
2026-09-22 11:01:38 +0700

    [zsh] add yazi shell wrapper that provides the ability to change cwd

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -12,6 +12,14 @@ if command -v zoxide &> /dev/null; then
     eval "$(zoxide init --cmd cd zsh)"
 fi
 
+function yy() {
+	local tmp cwd; tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd" || builtin true
+	command rm -f -- "$tmp"
+}
+
 if [[ -f "$ZSHCONFIG/zsh-autosuggestions/zsh-autosuggestions.zsh" ]]; then
     export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
     export ZSH_AUTOSUGGEST_USE_ASYNC=1
